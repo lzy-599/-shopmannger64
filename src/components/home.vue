18a2c5de72e6c4418ecf020ff92aafc4800314c5
lzy-599 <2365337537@qq.com>
2019-02-16 20:24:33 +0800

    引入布局容器

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>home-----</div>
+
+    <el-container class="container">
+        <el-header>Header</el-header>
+        <el-container>
+            <el-aside class="aside" width="200px">Aside</el-aside>
+            <el-main class="main">Main</el-main>
+        </el-container>
+    </el-container>
+
 </template>
 
 <script>
-
+export default{};
 </script>
 
 <style>
+    .container {
+        height: 100%;
+        background-color: #b3c0d1;
+    }
+    .aside {
+        background-color: yellow;
+    }
 
+    .main {
+        background-color: green;
+    }
 </style>
