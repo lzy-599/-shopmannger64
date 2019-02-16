c543d5aa63970d35518cf06cbe7da6c1fe1ed975
lzy-599 <2365337537@qq.com>
2019-02-16 13:20:13 +0800

    处理登录请求的响应

--- src/components/login.vue
+++ src/components/login.vue
@@ -8,26 +8,81 @@
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button class="login-btn" type="primary">登录</el-button>
+      <el-button @click.prevent="handleLogin()" class="login-btn" type="primary">登录</el-button>
     </el-form>
   </div>
 </template>
 
 <script>
-
-export default{
+export default {
     data () {
         return {
             formdata: {
                 username :'',
                 password :''
             }
-        }
+        };
+    },
+    methods: {
+      // 登录请求
+      handleLogin(){
+
+      
+        // 前提:api-server-> node app.js
+        // 请求没发送?
+        // 1. url写错?
+        // 2. axios导入失败?
+        // console.log(this.$http);
+        // 3. ?客户端一点问题没有 -> 服务端cmd卡死->重启->回车
+
+        // 目前代码: 异步操作的结果必须出现.then方法的里面-> 嵌套
+        // 希望: 不想在函数嵌套里面处理res
+        // 需要使用ES7的新特性: async await
+        // const res = this.$http.post();
+        // console.log(res);
+        this.$http
+          .post(`login`, this.formdata)
+          .then(res => {
+            console.log(res);
+            const {
+              data: {
+                data,
+                meta: { msg, status }
+              }
+            } = res;
+            if (status===200) {
+              console.log('success----');
+            } else {
+              // 提示框 -> UI
+              this.$message.error(msg);
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
     }
-}
+};
 
 </script>
 
 <style>
-
+.login-wrap {
+  /* 注意: 百分比布局 父元素height:100% */
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  background-color: #ffffff;
+  border-radius: 5px;
+  /* 开发 */
+  width: 400px;
+  padding: 30px;
+}
+.login-btn {
+  width: 100%;
+}
 </style>
