ca4cd6d3ace00e3f73f48c4fab7d3be75ae0724b
lzy-599 <2365337537@qq.com>
2019-02-18 21:14:49 +0800

    表格完成

--- src/components/users.vue
+++ src/components/users.vue
@@ -46,10 +46,18 @@
                 </template>
             </el-table-column>
             
-
-
-            <el-table-column prop="name" label="用户状态" width="140"></el-table-column>
-            <el-table-column prop="name" label="操作" width="200"></el-table-column>
+            <el-table-column label="用户状态" width="140">
+                <template slot-scope="scope"> 
+                    <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+                </template>
+            </el-table-column>
+            <el-table-column label="操作" width="200">
+                <template slot-scope="scope">
+                    <el-button type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
+                    <el-button type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
+                    <el-button type="success" icon="el-icon-check" circle size="mini" plain></el-button>
+                </template>
+            </el-table-column>
         </el-table>
 
     <!-- 分页 -->
