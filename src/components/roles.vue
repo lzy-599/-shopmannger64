42c01ae136753bafa60a82ab482fee3e8c807f9c
lzy-599 <2365337537@qq.com>
2019-02-28 20:46:41 +0800

    三级权限完成

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -1,41 +1,60 @@
 <template>
     <el-card class="box">
         <!-- 面包屑 -->
-         <cus-bread level1="权限管理" level2="角色列表"></cus-bread>
-         <el-button class="btn" type="primary">添加角色</el-button>
-         <!-- 表格 -->
-         <el-table height="250px" :data="roles" style="width: 100%">
-      
-      <el-table-column type="index" label="#" width="180"></el-table-column>
-      <el-table-column prop="roleName" label="角色名称" width="220"></el-table-column>
-      <el-table-column prop="roleDesc" label="角色描述" width="300"></el-table-column>
-      <el-table-column label="操作" width="200">
-        <template slot-scope="scope">
-          <el-button
-            type="primary"
-            icon="el-icon-edit"
-            circle
-            size="mini"
-            plain
-          ></el-button>
-          <el-button
-            type="danger"
-            icon="el-icon-delete"
-            circle
-            size="mini"
-            plain
-          ></el-button>
-          <el-button
-            @click="showDiaSetRights(scope.row)"
-            type="success"
-            icon="el-icon-check"
-            circle
-            size="mini"
-            plain
-          ></el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+        <cus-bread level1="权限管理" level2="角色列表"></cus-bread>
+        <el-button class="btn" type="primary">添加角色</el-button>
+        <!-- 表格 -->
+        <el-table height="350px" :data="roles" style="width: 100%">
+            <el-table-column type="expand" width="80">
+                <template slot-scope="scope">
+                    <!-- 行列布局 -->
+                    <el-row class="level1" v-for="(item1,i) in scope.row.children" :key="item1.id">
+                        <el-col :span="4">
+                            <el-tag closable type="success">
+                                {{item1.authName}}
+                            </el-tag>
+                            <!-- <i class="el-icon-arrow-right"></i> -->
+                        </el-col>
+                        <el-col :span="20">
+                            <el-row class="level2" v-for="(item2,i) in item1.children" :key="item2.id">
+                                <el-col :span="4">
+                                    <el-tag closable type="warning">
+                                        {{item2.authName}}
+                                    </el-tag>
+                                </el-col>
+                                <el-col :span="20">
+                                    <el-tag closable type="info" v-for="(item3,i) in item2.children" :key="item3.id">
+                                        {{item3.authName}}
+                                    </el-tag>
+                                </el-col>
+                            </el-row>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col v-if="scope.row.children.length===0">
+                            <span>“未分配权限”</span>
+                        </el-col>
+                    </el-row>
+                </template>
+            </el-table-column>
+            <el-table-column type="index" label="#" width="180"></el-table-column>
+            <el-table-column prop="roleName" label="角色名称" width="220"></el-table-column>
+            <el-table-column prop="roleDesc" label="角色描述" width="300"></el-table-column>
+            <el-table-column label="操作" width="200">
+                <template slot-scope="scope">
+                    <el-button type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
+                    <el-button type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
+                    <el-button
+                        @click="showDiaSetRights(scope.row)"
+                        type="success"
+                        icon="el-icon-check"
+                        circle
+                        size="mini"
+                        plain
+                    ></el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </el-card>
 </template>
 
@@ -53,6 +72,7 @@ export default {
         showDiaSetRights(){
 
         },
+        // 获取权限
         async getRoles(){
             const res = await this.$http.get(`roles`);
             console.log(res);
@@ -74,5 +94,9 @@ export default {
 }
 .btn {
     margin-top: 20px;
+}
+.level2,
+.level1 {
+    margin-top: 10px;
 }
 </style>
